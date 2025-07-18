c127a6251571f4fb459954504c0456bdd085f3d2
Sumith <sumith.kumarsinghp@abcsupply.com>
2025-07-18 17:51:11 +0530

    Deployment fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,24 +1,23 @@
-// index.js
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "react-oidc-context";
+import { BrowserRouter } from "react-router-dom";
 
 const cognitoAuthConfig = {
-  authority: "https://cognito-idp.ap-south-1.amazonaws.com/ap-south-1_W0ytLMKwS",
+  authority: "https://salonapp.auth.ap-south-1.amazoncognito.com", // ✅ Your Hosted UI Domain
   client_id: "1f6ch58jioma5ireu11astd3m1",
-  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net",
+  redirect_uri: "https://d84l1y8p4kdic.cloudfront.net", // ✅ Or http://localhost:3000 for local testing
   response_type: "code",
-  scope: "phone openid email",
+  scope: "openid email phone profile", // ✅ Full scopes
 };
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-// wrap the application with AuthProvider
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider {...cognitoAuthConfig}>
-      <App />
-    </AuthProvider>
+    <BrowserRouter>
+      <AuthProvider {...cognitoAuthConfig}>
+        <App />
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
